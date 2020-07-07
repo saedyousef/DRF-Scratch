83ddece463084670d7c783c267d5515f94d7a32b
Saed Yousef <saed.alzaben@gmail.com>
2020-07-07 20:58:08 +0300

    freezing packages

--- sitech/settings.py
+++ sitech/settings.py
@@ -125,6 +125,7 @@ STATIC_URL = '/static/'
 # DRF Settings
 
 REST_FRAMEWORK = {
+    'DEFAULT_SCHEMA_CLASS': 'rest_framework.schemas.coreapi.AutoSchema',
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 10
 }
--- sitech/urls.py
+++ sitech/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import path, include
 from rest_framework import routers
 from blog import views
+from rest_framework.documentation import include_docs_urls
 
 router = routers.DefaultRouter()
 router.register(r'articles', views.ArticleViewSet)
@@ -25,6 +26,6 @@ router.register(r'users', views.UserViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('rest_framework.urls', namespace='rest_framework')),
+    path('api/', include_docs_urls(title='Blog API', description='RESTful API for Blog')),
     path('', include(router.urls)),
 ]
